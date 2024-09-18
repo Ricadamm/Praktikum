63f0f2cf3e39d3e9d54b9cee9b9d8af0d5b3e5c1
Ricadamm <adamrizky742@gmail.com>
2024-09-18 12:44:36 +0700

    Update Problem.cpp

--- Week2/Problem1/Problem.cpp
+++ Week2/Problem1/Problem.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
+using namespace std;
 
+int main()
+{
+    cout << "Payslip for Employee:" << endl;
+    cout << "....................." << endl;
+
+
+    string Name;
+    cout << "Name: ";
+    cin >> Name ;
+
+    float Gross;
+    cout << "Gross Salary: " << "Rp";
+    cin >> Gross ;
+
+    float Tax = (0.2)*Gross;
+    cout << fixed << "Tax (20%):  " << "Rp" << Tax << endl;
+
+    float Installment;
+    cout << "Installment: " << "Rp";
+    cin >> Installment;
+
+    float Insurance;
+    cout << "Insurance: " << "Rp";
+    cin >> Insurance ;
+
+    float Net = Gross - Tax - Installment - Insurance;
+    cout << fixed << "Net Salary: " << "Rp " << Net;
+
+}
