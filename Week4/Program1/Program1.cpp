107064e6f50ae083a105b0d152125f1ae8c9346c
Ricadamm <adamrizky742@gmail.com>
2024-09-22 15:59:05 +0700

    Update Program1.cpp

--- Week4/Program1/Program1.cpp
+++ Week4/Program1/Program1.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int n, sum;
+
+    // Prompt the user to enter a positive integer
+    cout << "Enter a positive integer: ";
+    cin >> n;
+
+    // Calculate the sum of natural numbers up to n
+    for (int i = 1; i <= n; ++i) {
+        sum += i;
+    }
+
+    // Output the result
+    cout << "Sum = " << sum << endl;
+
+    return 0;
+}
