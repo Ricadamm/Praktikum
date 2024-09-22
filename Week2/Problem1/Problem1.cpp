ed6f65dfb3c2a9beb0f849fc95618b4e2fa20e89
Ricadamm <adamrizky742@gmail.com>
2024-09-22 15:54:09 +0700

    Update Problem1.cpp

--- Week2/Problem1/Problem1.cpp
+++ Week2/Problem1/Problem1.cpp
@@ -30,3 +30,29 @@ int main()
     cout << fixed << "Net Salary: " << "Rp " << Net;
 
 }
+
+int main() {
+    std::ifstream testFile("test.txt");
+    int input;
+    std::string expected_output;
+
+    if (!testFile) {
+        std::cerr << "Error: Could not open test.txt" << std::endl;
+        return 1;
+    }
+
+    int test_num = 1;
+    while (testFile >> input && std::getline(testFile >> std::ws, expected_output)) {
+        std::string output = Problem1(input);
+        if (output == expected_output) {
+            std::cout << "Test " << test_num << " passed!" << std::endl;
+        } else {
+            std::cout << "Test " << test_num << " failed. Expected: \"" 
+                      << expected_output << "\", Got: \"" << output << "\"" << std::endl;
+        }
+        test_num++;
+    }
+
+    testFile.close();
+    return 0;
+}
