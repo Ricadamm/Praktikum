01c6ca51e580037477a57cdd05c009cc5a64bbf0
Ricadamm <adamrizky742@gmail.com>
2024-09-22 16:32:26 +0700

    Update Problem1.cpp

--- Week2/Problem1/Problem1.cpp
+++ Week2/Problem1/Problem1.cpp
@@ -1,22 +1,28 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
+
+string Problem1(int input) {
+    return to_string(input);  
+}
 
 int main()
 {
+
     cout << "Payslip for Employee:" << endl;
     cout << "....................." << endl;
 
-
     string Name;
     cout << "Name: ";
-    getline(cin, Name);
+    getline(cin, Name);  
 
     float Gross;
     cout << "Gross Salary: " << "Rp";
-    cin >> Gross ;
+    cin >> Gross;
 
-    float Tax = (0.2)*Gross;
-    cout << fixed << "Tax (20%):  " << "Rp" << Tax << endl;
+    float Tax = 0.2 * Gross;
+    cout << fixed << "Tax (20%): " << "Rp" << Tax << endl;
 
     float Installment;
     cout << "Installment: " << "Rp";
@@ -24,16 +30,24 @@ int main()
 
     float Insurance;
     cout << "Insurance: " << "Rp";
-    cin >> Insurance ;
+    cin >> Insurance;
 
     float Net = Gross - Tax - Installment - Insurance;
-    cout << fixed << "Net Salary: " << "Rp " << Net;
+    cout << fixed << "Net Salary: " << "Rp " << Net << endl;
 
-}
 
-int test_num = 1;
+    ifstream testFile("test.txt");
+    int input;
+    string expected_output;
+
+    if (!testFile) {
+        cerr << "Error: Could not open test.txt" << endl;
+        return 1;
+    }
+
+    int test_num = 1;
     while (testFile >> input && getline(testFile >> ws, expected_output)) {
-        string output = Problem1(input);  // Call the function with input
+        string output = Problem1(input);  
         if (output == expected_output) {
             cout << "Test " << test_num << " passed!" << endl;
         } else {
@@ -45,3 +59,5 @@ int test_num = 1;
 
     testFile.close();
     return 0;
+}
+ 
