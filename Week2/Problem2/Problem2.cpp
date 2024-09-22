f1b0dcaf4ffb520f9a09dc84211ba2eebbf58a38
Ricadamm <adamrizky742@gmail.com>
2024-09-22 17:20:49 +0700

    Update Problem2.cpp

--- Week2/Problem2/Problem2.cpp
+++ Week2/Problem2/Problem2.cpp
@@ -4,15 +4,12 @@
 #include <cmath>
 using namespace std;
 
-string Problem2(int input){
-return to_string(input);
+string Problem2(int input) {
+    return to_string(input);
 }
-int main() 
-{
-    int a;
-    int b;
-    int c;
 
+int main() {
+    int a, b, c;
 
     cout << "Value of a: ";
     cin >> a;
@@ -21,36 +18,25 @@ int main()
     cout << "Value of c: ";
     cin >> c;
 
-   float D = b*b-(4*a*c);
-    cout << "D: b*b-(4ac) = " << D << endl;
+    float D = b * b - (4 * a * c);
+    cout << "D: b*b - (4ac) = " << D << endl;
 
-    if (D>0) {
-       float x1 = (-1*b + sqrt(D)) / (2*a);
-       float x2 = (-1*b - sqrt(D)) / (2*a);
-       cout << "D>0," << endl;
-       cout << "Result: x1 = "<< x1 <<endl;
-       cout << "Result: x2 = "<< x2 <<endl;
-        
-    }
-    
-
-    if  (D==0) {
-       float x = (-b)/2*a;
-       cout << "D=0," << endl;
-       cout << "Result: x = " << x << endl;
-        
-    }
-    
-
-   if (D<0) {
-        cout << "D<0, "<< endl;
+    if (D > 0) {
+        float x1 = (-b + sqrt(D)) / (2 * a);
+        float x2 = (-b - sqrt(D)) / (2 * a);
+        cout << "D > 0," << endl;
+        cout << "Result: x1 = " << x1 << endl;
+        cout << "Result: x2 = " << x2 << endl;
+    } else if (D == 0) {
+        float x = -b / (2.0 * a); // Corrected with parentheses
+        cout << "D = 0," << endl;
+        cout << "Result: x = " << x << endl;
+    } else { // D < 0
+        cout << "D < 0," << endl;
         cout << "Result: There are no real roots" << endl;
-       
-   }
-   
-}
+    }
 
-
+    // File reading for tests
     std::ifstream testFile("test.txt");
     int input;
     std::string expected_output;
@@ -61,7 +47,9 @@ int main()
     }
 
     int test_num = 1;
-    while (testFile >> input && std::getline(testFile >> std::ws, expected_output)) {
+    while (testFile >> input) {
+        testFile.ignore(); // Ignore the newline character
+        std::getline(testFile, expected_output); // Read the expected output
         std::string output = Problem2(input);
         if (output == expected_output) {
             std::cout << "Test " << test_num << " passed!" << std::endl;
