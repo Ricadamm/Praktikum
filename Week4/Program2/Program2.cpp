d5d9819588a5951ff800b7850afb0952836bf8d2
Ricadamm <adamrizky742@gmail.com>
2024-09-22 16:00:48 +0700

    Update Program2.cpp

--- Week4/Program2/Program2.cpp
+++ Week4/Program2/Program2.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 
 int main() {
-    int n;
+    int n, i;
 
    
     cout << "Enter an integer: ";
     cin >> n;
 
    
-    for (int i = 1; i <= 10; ++i) {
+    for (i = 1; i <= 10; ++i) {
         cout << n << " x " << i << " = " << n * i << endl;
     }
 
