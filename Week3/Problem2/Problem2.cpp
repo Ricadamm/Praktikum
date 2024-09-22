3eee78c5858e6e2c997a70970c59f6939a3b4391
Ricadamm <adamrizky742@gmail.com>
2024-09-22 17:55:24 +0700

    Update Problem2.cpp

--- Week3/Problem2/Problem2.cpp
+++ Week3/Problem2/Problem2.cpp
@@ -22,11 +22,11 @@ int main()
     
     double largest;
     
-    if (num1 > num2 && num1 > num3)
+    if (num1 >= num2 && num1 >= num3)
     {
         largest = num1;
     }
-    else if (num2 > num1 && num2 > num3)
+    else if (num2 >= num1 && num2 >= num3)
     {
         largest = num2;
     }
