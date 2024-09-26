6a5ec01d186904ab1cf0643e6a7e41a3b67a76e8
Ricadamm <adamrizky742@gmail.com>
2024-09-26 17:02:49 +0700

    Update Program2.cpp

--- Week5/Program2/Program2.cpp
+++ Week5/Program2/Program2.cpp
@@ -3,17 +3,17 @@
 using namespace std;
 
 int main() {
-    int n = 5;
-    vector<int> vec(n);
+    int n = 5; // gives n the value of 5
+    vector<int> vec(n); // creating vector with the size n
 
-    cout << "Enter the elements: ";
-    for (int i = 0; i < n; ++i) {
-        cin >> vec[i];
+    cout << "Enter the elements: "; // Asks the user to input how many elements
+    for (int i = 0; i < n; ++i) { // Loop to take input from the user 
+        cin >> vec[i]; // Stores the input value at at index i
     }
 
-   cout << "Reversed vector: ";
-    for (int i = n - 1; i >= 0; --i) {
-        cout << vec[i] << " ";
+   cout << "Reversed vector: "; // Display the result of the reversed vector
+    for (int i = n - 1; i >= 0; --i) { // Loops the vector but does it from the last index to the first index
+        cout << vec[i] << " "; // outputs the result
     }
     cout << endl;
 
