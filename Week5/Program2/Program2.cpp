495073cc7f8b91a9cb605b0862e697c8dd7ccc16
Ricadamm <adamrizky742@gmail.com>
2024-09-26 14:33:31 +0700

    Update Program2.cpp

--- Week5/Program2/Program2.cpp
+++ Week5/Program2/Program2.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
 
+int main() {
+    int n = 5;
+    vector<int> vec(n);
+
+    cout << "Enter the elements: ";
+    for (int i = 0; i < n; ++i) {
+        cin >> vec[i];
+    }
+
+   cout << "Reversed vector: ";
+    for (int i = n - 1; i >= 0; --i) {
+        cout << vec[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
