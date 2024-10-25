f26f9b9040011e0f95fc49aec71d48af8c4d31ad
Ricadamm <adamrizky742@gmail.com>
2024-10-25 14:54:48 +0700

    Update Problem1.cpp

--- Week7/Problem1/Problem1.cpp
+++ Week7/Problem1/Problem1.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+using namespace std;
 
+void calculate(int a, int b, char operation) {
+    if (operation == '+') {
+        cout << "Result: " << (a + b) << endl;
+    } else if (operation == '-') {
+        cout << "Result: " << (a - b) << endl;
+    } else {
+        cout << "Wrong operation" << endl;
+    }
+}
+
+int main() {
+    int a, b;
+    char operation;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+    cout << "Enter operation: ";
+    cin >> operation;
+  
+    return 0;
+}
