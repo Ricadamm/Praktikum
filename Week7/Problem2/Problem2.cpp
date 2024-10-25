756dde853de3a420fec0beabf7caa0feaecbcddf
Ricadamm <adamrizky742@gmail.com>
2024-10-25 14:49:38 +0700

    Update Problem2.cpp

--- Week7/Problem2/Problem2.cpp
+++ Week7/Problem2/Problem2.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+using namespace std;
 
+int factorial(int x) {
+    if (x <= 1) {
+        return 1; 
+    } else {
+        return x * factorial(x - 1);
+    }
+}
+
+int main() {
+    int x;
+    cout << "Enter a number to calculate its factorial: ";
+    cin >> x; 
+    cout << "Factorial of " << x << " is " << factorial(x) << endl; 
+    return 0;
+}
